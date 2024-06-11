df4d00aad6ce1515ac659ed1292693df040cfa16
Anuraag Thakur <115446867+AnuraagThakur12222078@users.noreply.github.com>
2024-06-11 20:25:49 +0530

    Add files via upload

---
+++ mergetwoarray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+        for(int i=0;i<n;i++)
+        {
+            nums1[m+i]=nums2[i];
+
+        }
+       
+        sort(nums1.begin(),nums1.end());
+        cout<<"[";
+        for(int i=0;i<nums1.size();i++)
+        {
+            cout<<nums1[i]<<",";
+        }
+        cout<<"]";
+    }
+};
