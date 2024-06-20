da988da1c6b7d73603b575a0760fe13bb8a4457b
Anuraag Thakur <115446867+AnuraagDev@users.noreply.github.com>
2024-06-20 12:57:41 +0530

    Create asteroidCollision.cpp

---
+++ asteroidCollision.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {stack<int> stack;
+   for(int a:asteroids){
+    if(a>0){
+        stack.push(a);
+    }else{
+        while(!stack.empty()&& stack.top()>0&&stack.top()<-a)stack.pop();
+    if(stack.empty()||stack.top()<0)stack.push(a);
+    if(!stack.empty()&&stack.top()==-a)stack.pop();
+    }
+   }
+   vector<int> res(stack.size());
+   int i=stack.size()-1;
+   while(!stack.empty()){
+
+   res[i--]=stack.top();
+   stack.pop();
+        
+    }
+    return res;}
+};
